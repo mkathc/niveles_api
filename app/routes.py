1620205333dd8117726aa0d910473afeb7526e49
Katherine Caillahua <mcaillahua@belatrixsf.com>
2020-08-23 14:05:42 -0500

    update resources

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import request, Response, jsonify
 from bson import json_util
 from bson.objectid import ObjectId
 from bson.json_util import dumps
-from router import app, mongo
+from main import app, mongo
 import random
 import json
 
